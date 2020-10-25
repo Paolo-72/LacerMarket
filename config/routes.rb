bf5781794e23b884846f4cefb4bf16a80de68256
Paolo-72 <past161@alice.it>
2020-10-25 19:35:26 +0100

    creata homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  #Il comando get crea un link 'pages/home'
+  #get 'pages/home'
+  #Il comando root crea la pagina di riferimento(pages#home) impostando il controller pages e l'azione home
+  root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
