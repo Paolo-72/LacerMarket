b394c8fded9c667938de0a5d4532cbe9f4cd44fd
Paolo-72 <past161@alice.it>
2020-10-27 19:54:48 +0100

    aggiunta pagina di about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,11 @@
 class PagesController < ApplicationController
+  
+  #Definizione controller home
   def home
   end
+
+  #Definizione controller about
+  def about
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   #get 'pages/home'
   #Il comando root crea la pagina di riferimento(pages#home) impostando il controller pages e l'azione home
   root 'pages#home'
+  #Il comando get crea il link per la pagina di about nel controller pages azione home
+  get 'about' => 'pages#about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
