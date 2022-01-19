76856c21a0fa05a39280eb5fb45c96c262822ac1
Paolo-72 <past161@alice.it>
2022-01-19 17:26:41 +0100

    creata homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  #Imposto la pagina iniziale
+  #get 'pages/home'
+  root 'pages#home'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
